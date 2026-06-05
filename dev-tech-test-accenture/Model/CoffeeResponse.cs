b7b0a54651e528fcf5a676a055fa64c38523a6e6
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2026-06-05 14:33:14 +0800

    :whale: dockerize, cleaned project (#1)

---
+++ dev-tech-test-accenture/Model/CoffeeResponse.cs
@@ -0,0 +1,7 @@
+namespace dev_tech_test_accenture.Models;
+
+public class CoffeeResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string Prepared { get; set; } = string.Empty;
+}
